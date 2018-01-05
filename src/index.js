caf30ba2739721134b0bb9cd071481073d548fa2
Marcos Pinilla <marcos@iMac-de-DCIUFRO.local>
2018-01-05 14:51:10 -0300

    dashboard 2.0

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ angular
       .accentPalette('lime')
       .warnPalette('red');
       $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+      $mdThemingProvider.theme('dark-green').backgroundPalette('green').dark();
   	  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
       $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
       $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
