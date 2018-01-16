f3dd5391501287f766abdbb4237188d8d91f3d31
Marcos Pinilla <marcos@iMac-de-DCIUFRO.local>
2018-01-16 12:54:37 -0300

    detalles en crud profesor

--- src/app/components/profesor/profesor.component.js
+++ src/app/components/profesor/profesor.component.js
@@ -9,7 +9,25 @@
     controllerAs: 'vm'
   });
 
-  function profesorCtrl() {
+  profesorCtrl.$inject = ['$mdDialog']
+
+  function profesorCtrl($mdDialog) {
     var vm = this;
+
+    vm.people = [
+      { name: 'Jorge Hoschtetter', img: 'img/100-0.jpeg', newMessage: true },
+      { name: 'Roberto Espinosa', img: 'img/100-1.jpeg', newMessage: false },
+      { name: 'Adrian Herrera', img: 'img/100-2.jpeg', newMessage: false }
+    ];
+    vm.goToPerson = function(person, event) {
+      $mdDialog.show(
+        $mdDialog.alert()
+        .title('Navigating person')
+        .textContent('Inspect ' + person)
+        .ariaLabel('Person inspect demo')
+        .ok('Neat!')
+        .targetEvent(event)
+      );
+    };
   }
 })();
