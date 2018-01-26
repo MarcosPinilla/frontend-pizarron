9b1ed123eb7e54ac3343c52945c50ed93c7b6127
Marcos Pinilla <m.pinilla03@ufromail.cl>
2018-01-26 09:38:13 -0300

    subida elemento

--- src/app/components/elemento/elemento.component.js
+++ src/app/components/elemento/elemento.component.js
@@ -7,9 +7,35 @@
     templateUrl: 'app/components/elemento/elemento.html',
     controller: elementoCtrl,
     controllerAs: 'vm'
-  });
+  })
+  .directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+  }]);
 
   function elementoCtrl() {
     var vm = this;
+
+    vm.elemento={};
+
+    vm.crear = function (modelo){
+      console.log(modelo);
+    };
+
+
   }
 })();
