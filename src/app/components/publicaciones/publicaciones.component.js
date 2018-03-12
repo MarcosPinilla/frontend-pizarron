33ba75d2382b8c8c070ea1660adaade8abe14bbc
Jorge Solis <jsolisdev@gmail.com>
2018-03-12 11:38:01 -0300

    Comentarios y publicaciones en dashboard implementados

--- src/app/components/publicaciones/publicaciones.component.js
+++ src/app/components/publicaciones/publicaciones.component.js
@@ -9,9 +9,35 @@
     	controllerAs: 'vm'
   	});
 
-  	//publicacionesCtrl.$inject = ['CredentialsService', '$rootScope', '$state'];
+  	publicacionesCtrl.$inject = ['MaterialService', 'ComentarioService', '$state'];
 
-  	function publicacionesCtrl() {
+  	function publicacionesCtrl(MaterialService, ComentarioService) {
   		var vm = this;
+      vm.materiales = {};
+      vm.comentarios = {};
+      vm.comentario;
+
+      var comentario = JSON.parse('{"idMaterial": ' + 1 + '}');
+      //console.log(comentario);
+
+      MaterialService.query().$promise.then(function (data) {
+        vm.materiales = data;
+        console.log(vm.materiales);
+        console.log(vm.materiales.materiales);
+      });
+
+      vm.verDocumento = function() {
+        console.log('ver documento!');
+      }
+
+      vm.agregarFavoritos = function() {
+        console.log('Agregado a favoritos');
+      }
+
+      vm.comentar = function(idmaterial, texto) {
+        var coment = JSON.parse('{"id_material": ' + idmaterial + ', "comentario": ' + '"' + texto + '"' + '}');
+        console.log(coment);
+        ComentarioService.save(coment);
+      }
   	}
 })();
