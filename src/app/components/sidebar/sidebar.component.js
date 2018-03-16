8f00b6987bed3adcf3483c4ce4a2432cb14c44bc
Jorge Solis <jsolisdev@gmail.com>
2018-03-16 16:58:40 -0300

    Vista nueva

--- src/app/components/sidebar/sidebar.component.js
+++ src/app/components/sidebar/sidebar.component.js
@@ -9,10 +9,10 @@
     	controllerAs: 'vm'
   	});
 
-  	sidebarCtrl.$inject = ['$mdDialog',  'CantidadNotificaciones','ListarasignaturasService', 'ListarnivelesService', 'ListartipomaterialService',
+  	sidebarCtrl.$inject = ['$mdDialog', '$pusher', 'CantidadNotificaciones','ListarasignaturasService', 'ListarnivelesService', 'ListartipomaterialService',
      'PerfilService', 'MaterialService', 'NotificacionesNoLeidasService', 'CambiarNotificacionesLeidas', 'NotificacionesLeidasService'];
 
-  	function sidebarCtrl($mdDialog, CantidadNotificaciones, ListarasignaturasService, ListarnivelesService, ListartipomaterialService, 
+  	function sidebarCtrl($mdDialog, $pusher ,CantidadNotificaciones, ListarasignaturasService, ListarnivelesService, ListartipomaterialService, 
       PerfilService, NotificacionesNoLeidasService) {
   		var vm = this;
 
@@ -27,6 +27,7 @@
       vm.customFullscreen = true;
       
        PerfilService.get().$promise.then(function (data) {
+          console.log(data);
           vm.perfil = data;
           vm.nombre = vm.perfil.nombres_profesor;
           vm.nombre = vm.nombre.split(" ");
@@ -52,7 +53,21 @@
             vm.cantidadNotificaciones = data;
 
        });
+//socket 
+       var client = new Pusher('28705022aa554d22c965', {
+          cluster: 'us2',
+          // authEndpoint: "http://example.com/pusher/auth",
+          encrypted: true
+        });
 
+       var pusher = $pusher(client);
+
+       var canal = pusher.subscribe('notificacion');
+
+       canal.bind('NotificacionesEvent',
+        function (data) {
+          console.log(data);
+        });
 
 
       
