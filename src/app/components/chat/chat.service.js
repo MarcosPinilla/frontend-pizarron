b76c562784b2e73c8d7366b2b6548fa32b6cf8b5
Vichoh <a.herrera04@ufromail.cl>
2018-04-09 10:00:01 -0300

    arreglo chat

---
+++ src/app/components/chat/chat.service.js
@@ -0,0 +1,13 @@
+(function () {
+	'use strict';
+
+	angular
+	.module('app')
+	.service('ChatService', chatService);
+
+	chatService.$inject = ['$resource', 'API'];
+
+	function chatService($resource, API){
+		return $resource(API + 'send');
+	}
+})();
