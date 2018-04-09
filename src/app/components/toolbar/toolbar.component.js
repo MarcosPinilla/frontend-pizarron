8ce22c3047c40714828d46b56e227504dca9f3ca
Jorge Solis <jsolisdev@gmail.com>
2018-04-08 21:25:33 -0300

    Arreglos en vista general

--- src/app/components/toolbar/toolbar.component.js
+++ src/app/components/toolbar/toolbar.component.js
@@ -9,10 +9,15 @@
     controllerAs: 'vm'
   });
 
-  toolbarCtrl.$inject = ['CredentialsService', '$rootScope', '$state','$window', 'DarFavorito'];
+  toolbarCtrl.$inject = ['CredentialsService', '$rootScope', '$mdDialog', '$state', '$window', 'DarFavorito', 'ListarasignaturasService', 'ListarnivelesService', 'ListartipomaterialService'];
 
-  function toolbarCtrl(CredentialsService, $rootScope, $state, $window, DarFavorito) {
+  function toolbarCtrl(CredentialsService, $rootScope, $mdDialog, $state, $window, DarFavorito, ListarasignaturasService, ListarnivelesService, ListartipomaterialService) {
     var vm = this;
+
+    vm.usuario = localStorage.getItem("user");
+    vm.asignaturas = {};
+    vm.niveles = {};
+    vm.tipo_material = {};
 
     vm.isinLogged = false;
 
@@ -35,6 +40,19 @@
       vm.isinLogged = true;
     });
 
+    /*Lista las asignaturas*/
+    ListarasignaturasService.query().$promise.then(function (data) {
+      vm.asignaturas = data;
+    });
+
+    ListarnivelesService.query().$promise.then(function (data) {
+      vm.niveles = data;
+    });
+
+    ListartipomaterialService.query().$promise.then(function (data) {
+      vm.tipo_material = data;
+    });
+
     vm.gohome = function () {
       vm.isinLogged = false;
       $state.go('landing');
@@ -44,11 +62,76 @@
       $window.history.back()
     };
 
+    vm.showNewDocument = function (ev, usuario, asignaturas, niveles, tipomaterial) {
+        $mdDialog.show({
+          controller: dialogoController,
+          controllerAs: 'vm',
+          templateUrl: 'app/components/dashboard/nuevodocumento.dialogo.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose: true,
+          fullscreen: vm.customFullscreen, // Only for -xs, -sm breakpoints.
+          locals: {
+            usuario: usuario,
+            asignaturas: asignaturas,
+            niveles: niveles,
+            tipomaterial: tipomaterial
+          },
+        })
+        .then(function (answer) {
+          vm.status = 'Documento:  ' + answer + '.';
+        }, function () {
+          vm.status = 'CANCELADO';
+        });
+    };
 
+    function dialogoController($mdDialog, usuario, asignaturas, niveles, tipomaterial, $state, MaterialService) {
+      var vm = this;
+      vm.usuario = usuario;
+      vm.asignaturas = asignaturas;
+      vm.niveles = niveles;
+      vm.tipo_materiales = tipomaterial;
+
+      vm.material = {};
+      vm.material_id = '';
+
+      /*console.log(vm.usuario);
+      console.log(vm.asignaturas);
+      console.log(vm.niveles);
+      console.log(vm.tipo_materiales);*/
+
+      vm.crearmaterial = function (material) {
+        if(material.titulo_material != null && material.id_asignatura != null && material.id_nivel != null && material.id_tipo_material != null && vm.material.id_visibilidad != null) {
+          //console.log(material);
+          MaterialService.save(material, function (res){
+            console.log(res);
+            vm.material_id = res.id;
+            $state.go('editdocument', {id: res.id});
+            $mdDialog.hide();
+          },function (err) {
+            console.log(err);
+          });
+       
+          //$state.go('editdocument');
+        }
+      };     
+
+      vm.hide = function () {
+        $mdDialog.hide();
+      };
+
+      vm.cancel = function () {
+        $mdDialog.cancel();
+      };
+
+      vm.answer = function (answer) {
+        $mdDialog.hide(answer);
+      };
+    }
 
     vm.material = {
-        material_id: "2"
-      }
+      material_id: "2"
+    }
 
 
       vm.darFavorito = function(data){
