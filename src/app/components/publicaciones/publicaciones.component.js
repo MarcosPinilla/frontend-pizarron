5ac99ff08e9264ad4fd5232f86bb07eff3cf857d
Jorge Solis <jsolisdev@gmail.com>
2018-04-18 16:52:04 -0300

    Fixes en dashboard

--- src/app/components/publicaciones/publicaciones.component.js
+++ src/app/components/publicaciones/publicaciones.component.js
@@ -24,12 +24,6 @@
         vm.materiales = data;
         console.log(vm.materiales);
         console.log(vm.materiales.materiales);
-
-        setTimeout(function() {
-          for(let i = 0; i < vm.materiales.length; i++) {
-            document.getElementById(vm.materiales[i].id).innerHTML = "<center><a ui-sref='editdocument({id:" + vm.materiales[i].id + "})'>" + vm.materiales[i].vista_previa + "</a></center>";  
-          }
-        }, 300);
       });
 
       vm.verDocumento = function() {
