07965d00ae10ae2793df0960d4d38cbc1775d841
Cristobal Berrios <elcriss96@gmail.com>
2018-10-22 14:42:20 -0300

    se cambia a puerto 3000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app = express();
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log('server started '+ port);
